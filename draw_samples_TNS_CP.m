39e3a3a92d66bfd0e21b784b678f8fe1b15a56d6
Osman Asif Malik <osman.malik.87@gmail.com>
2022-09-20 14:34:59 -0700

    Add existing code

---
+++ draw_samples_TNS_CP.m
@@ -0,0 +1,197 @@
+function [samples, sqrt_probs] = draw_samples_TNS_CP(A, AtA, n, no_samp)
+%draw_samples_CP Draws samples for CP design matrix as discussed in our
+%paper
+%
+%[samples, sqrt_probs] = draw_samples_CP(A, sketch, n, no_samp) returns
+%no_samp samples organized into an no_samp by N matrix, where N is the
+%number of modes of the tensor being decomposed, and a vector sqrt_probs
+%which contains the square root of the probability of drawing each of the
+%sampled rows. The cell A should contain all the CP factor matrices in
+%standard order, and the n-th factor matrix is the one being solved for.
+%The n-th column of samples will just be NaN, since that index is not being
+%sampled.
+
+N = length(A);
+
+% if statement to avoid issues when n=1 and A{1} is empty
+if n == 1
+    R = size(A{2},2);
+else 
+    R = size(A{1},2);
+end
+
+% Compute matrix Phi
+AmTAm = ones(R, R);
+for j = 1:N
+    if j ~= n
+        AmTAm = AmTAm .* AtA{j};
+    end
+end
+Phi = pinv(AmTAm);
+
+% Precompute the two terms in Eq (28) in paper
+M1 = cell(1,N);
+M2 = cell(1,N);
+for j = 1:N
+    if j ~= n
+        M1{j} = khatrirao(A{j}.', A{j}.');
+        M2{j} = A{j}.' * A{j};
+    end
+end
+
+% Also precompute the products in the second of those terms.
+% Note that these products incorporate the Phi matrix as well.
+term_2 = cell(1,N);
+for m = 1:N
+    term_2{m} = Phi(:);
+    %term_2{m} = ones(numel(Phi),1);
+    for j = m+1:N
+        if j ~= n
+            term_2{m} = term_2{m} .* M2{j}(:);
+        end
+    end
+end
+
+% Main loop for drawing all samples
+samples = nan(no_samp, N); % Each row is a realization (i_j)_{j \neq n}
+sqrt_probs = ones(no_samp, 1); % To store the square root of the probability of each drawn sample
+if n == 1
+    first_idx = 2;
+else
+    first_idx = 1;
+end
+
+%% Draw first subindex for each sample
+
+m = first_idx;
+
+% Compute conditional probability vector
+%common_terms = Phi(:);
+%for j = 1:N
+%    if j > m && j ~= n
+%        common_terms = common_terms .* M2{j}(:);
+%    end
+%end
+%common_terms = common_terms.';
+
+common_terms = term_2{m}.';
+
+%const = common_terms * M2{m}(:);
+%common_terms = common_terms / const;
+prob_m = common_terms * M1{m};
+prob_m = prob_m / sum(prob_m);
+
+% Draw from the vector
+samples(:, m) = randsample(length(prob_m), no_samp, true, prob_m);
+
+% Update probability vector
+sqrt_probs = sqrt(prob_m(samples(:, m)));
+
+
+%% Compute rest of subindices
+
+for samp = 1:no_samp
+    % Compute P(i_m | (i_j)_{j < m, ~= n}) for each m (~=n) and draw i_m 
+    current_sample = samples(samp, :);
+    temp_common_terms = M1{first_idx}(:, current_sample(first_idx));
+    for m = first_idx+1:N
+        if m ~= n
+            
+            % Compute conditional probability vector
+            %common_terms = Phi(:);
+            %for j = 1:N
+            %    if j < m && j ~= n
+            %        %common_terms = common_terms .* M1{j}(:, samples(samp, j));
+            %        common_terms = common_terms .* M1{j}(:, current_sample(j));
+            %    elseif j > m && j ~= n
+            %        common_terms = common_terms .* M2{j}(:);
+            %    end
+            %end
+
+            %{
+            common_terms = Phi(:);
+            for j = 1:m-1
+                if j ~= n
+                    common_terms = common_terms .* M1{j}(:, current_sample(j)); 
+                end
+            end
+            common_terms = common_terms .* term_2{m};
+
+            common_terms_2 = temp_common_terms .* term_2{m};
+            asdf = norm(common_terms - common_terms_2);
+            if asdf ~= 0
+                asdf
+            end  
+           %}
+
+            common_terms = (temp_common_terms .* term_2{m}).';
+            %common_terms = common_terms.';
+            prob_m = common_terms * M1{m};
+            prob_m = prob_m / sum(prob_m);
+
+            % Draw from the vector
+            sampled_idx = mt19937ar(prob_m);
+            %samples(samp, m) = sampled_idx;
+            current_sample(m) = sampled_idx;
+            temp_common_terms = temp_common_terms .* M1{m}(:, current_sample(m));  
+
+            % Update probability vector
+            sqrt_probs(samp) = sqrt_probs(samp) * sqrt(prob_m(sampled_idx));
+
+            
+        end
+    end
+    samples(samp,:) = current_sample;
+end
+
+
+%{
+first_idx_flag = true;
+for samp = 1:no_samp
+    % Compute P(i_m | (i_j)_{j < m, ~= n}) for each m (~=n) and draw i_m 
+    for m = first_idx:N
+        if m == first_idx && ~first_idx_flag
+            continue
+        end
+        
+        if m ~= n
+            
+            % Compute conditional probability vector
+            common_terms = Phi(:);
+            for j = 1:N
+                if j < m && j ~= n
+                    common_terms = common_terms .* M1{j}(:, samples(samp, j));
+                elseif j > m && j ~= n
+                    common_terms = common_terms .* M2{j}(:);
+                end
+            end
+            common_terms = common_terms.';
+            %const = common_terms * M2{m}(:);
+            %common_terms = common_terms / const;
+            prob_m = common_terms * M1{m};
+            prob_m = prob_m / sum(prob_m);
+
+            if first_idx_flag
+                % Draw from the vector
+                samples(:, m) = randsample(length(prob_m), no_samp, true, prob_m);
+                
+                % Update probability vector
+                sqrt_probs = sqrt(prob_m(samples(:, m)));
+                
+                first_idx_flag = false;
+            else
+                % Draw from the vector
+                samples(samp, m) = mt19937ar(prob_m);
+                
+                % Update probability vector
+                sqrt_probs(samp) = sqrt_probs(samp) * sqrt(prob_m(samples(samp,m)));
+            end
+            
+        end
+    end
+end
+%}
+
+sqrt_probs = sqrt_probs';
+
+end
---
+++ draw_samples_TNS_TR.m
@@ -0,0 +1,144 @@
+function [samples, sqrt_probs] = draw_samples_TNS_TR(G, n, J2)
+%draw_samples_TR Draws samples for TR design matrix as discussed in our
+%paper
+%
+%[samples, sqrt_probs] = draw_samples_TR(G, sketch, n, no_samp) returns
+%no_samp samples organized into a no_samp by N matrix, where N is the
+%number of modes of the tensor being decomposed, and a vector sqrt_probs
+%which contains the square root of the probability of drawing each of the
+%sampled rows. The cell G should contain all the TR core tensors in
+%standard order, and the n-th core is the one being solved for. The n-th
+%column of samples will just be NaN, since that index is not being sampled.
+%
+%Note that this function requires some files from the tr-als-sampled
+%repo which is available at: https://github.com/OsmanMalik/tr-als-sampled
+
+N = length(G);
+
+% Precompute the two terms in Eq (38) in paper, but reshaped into tensors
+% according to the discussion in Remark 20 of the paper
+C1 = cell(1,N);
+C1_unf = cell(1,N);
+C2 = cell(1,N);
+C2_vec = cell(1,N);
+for j = 1:N
+    [R0, I1, R1] = size(G{j});
+    if j ~= n
+        % Appropriate tensorization of first term
+        G_2 = mode_unfolding(G{j}, 2);
+        temp = khatrirao(G_2.', G_2.');
+        temp = reshape(temp, R1, R0, R1, R0, I1);
+        temp = permute(temp, [2 4 5 1 3]);
+        C1{j} = reshape(temp, R0^2, I1, R1^2);
+        C1_unf{j} = classical_mode_unfolding(C1{j}, 2);
+        
+        % Appropriate tensorization of second term
+        temp = G_2.' * G_2;
+        temp = reshape(temp, R1, R0, R1, R0);
+        temp = permute(temp, [2 4 1 3]);
+        C2{j} = reshape(temp, R0^2, R1^2);
+        C2_vec{j} = C2{j}(:).';        
+    end
+end
+
+% Precompute vectors with order in which to multiply cores.
+% For some reason, reinitializing the vector below ends up taking some
+% non-negligible amount of time if it's done inside the main loop below, so
+% predefining them here instead to avoid that.
+idx_order = cell(N,1);
+for m = 1:N
+    idx_order{m} = [m+1:N 1:m-1];
+end
+
+% Compute the Phi matrix, appropriately reshape it, and store in
+% C2{j}---this is along the same lines as what draw_samples_TR.m does, but
+% inside of the 1st for loop above. I.e., we store permuted Phi in with 
+% (G_[2]^(j))^T G_[2]^(j) matrices.
+AmTAm = C2{idx_order{n}(1)};
+for j = 2:N-1
+    AmTAm = AmTAm * C2{idx_order{n}(j)};
+end % After loop, AmTAm is (R_n*R_n) by (R_{n-1}*R_{n-1}) in size.
+[R0, ~, R1] = size(G{n});
+AmTAm = reshape(AmTAm, R1, R1, R0, R0);
+AmTAm = permute(AmTAm, [3 1 4 2]);
+AmTAm = reshape(AmTAm, R0*R1, R0*R1); % At this point, the quantity AmTAm is equal to what we call A^{\neq m \top} A^{\neq m} in the paper.
+Phi = pinv(AmTAm);
+temp = reshape(Phi, R0, R1, R0, R1);
+temp = permute(temp, [1 3 2 4]);
+C2{n} = reshape(temp, R0^2, R1^2);
+
+samples = nan(J2, N); % Each row is a realization (i_j)_{j \neq n}
+sqrt_probs = ones(J2, 1); % To store the square root of the probability of each drawn sample
+if n == 1
+    first_idx = 2;
+else
+    first_idx = 1;
+end
+first_idx_flag = true;
+
+% Main loop for drawing all samples
+for samp = 1:J2
+    % Compute P(i_m | (i_j)_{j < m, ~= n}) for each m (~=n) and draw i_m
+    for m = first_idx:N
+        if m == first_idx && ~first_idx_flag
+            continue
+        end
+        
+        if m ~= n
+            
+            % Compute conditional probability vector
+            idx = idx_order{m};
+            if idx(1) >= m || idx(1) == n
+                M = C2{idx(1)};
+            else
+                sz = size(C1{idx(1)}, 1:3);
+                M = reshape(C1{idx(1)}(:, samples(samp,idx(1)), :), sz(1), sz(3));
+            end
+            for j = 2:length(idx)
+                if idx(j) >= m || idx(j) == n
+                    M = M * C2{idx(j)};
+                else
+                    sz = size(C1{idx(j)}, 1:3);
+                    M = M * reshape(C1{idx(j)}(:, samples(samp,idx(j)), :), sz(1), sz(3));
+                end
+            end
+            common_terms = M';
+            
+            common_terms_vec = common_terms(:);
+            %const = C2_vec{m} * common_terms_vec;
+            %common_terms_vec = common_terms_vec / const;
+            prob_m = C1_unf{m} * common_terms_vec;
+            prob_m = prob_m / sum(prob_m);      
+
+            if first_idx_flag
+                % Draw from the vector
+                if min(prob_m)<0
+                    prob_m = max(prob_m, 0);
+                end
+                if sum(isnan(prob_m))>0
+                    error("Probability vector contains NaN")
+                end
+                samples(:, m) = randsample(length(prob_m), J2, true, prob_m);
+
+                % Update probability vector
+                sqrt_probs = sqrt(prob_m(samples(:, m)));
+                
+                first_idx_flag = false;
+            else
+                % Draw from the vector
+                if min(prob_m)<0
+                    prob_m = max(prob_m, 0);
+                end
+                if sum(isnan(prob_m))>0
+                    error("Probability vector contains NaN")
+                end
+                samples(samp, m) = mt19937ar(prob_m);
+                
+                % Update probability vector
+                sqrt_probs(samp) = sqrt_probs(samp) * sqrt(prob_m(samples(samp,m)));
+            end
+        end
+    end
+end
+
+end
